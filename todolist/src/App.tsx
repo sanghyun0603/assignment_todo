fee23eb28b6db58e0a7480f9645df0d4877be136
배상현 <tkdgusdl63@naver.com>
2024-03-01 21:12:47 +0900

    파일분리

---
+++ todolist/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoList from "./components/List/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TodoList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
